c34f9bfcca101dc376ea2c6f9ef940cf2096a400
kasper-pawlowski <kapi28000@gmail.com>
2023-02-02 23:16:17 +0100

    h2 > h3

--- src/pages/InfoPage/InfoPage.jsx
+++ src/pages/InfoPage/InfoPage.jsx
@@ -8,21 +8,21 @@ const InfoPage = () => {
             <h1>Quiz Architektoniczy</h1>
             <InfoWrapper>
                 <InfoSection>
-                    <h2>Lorem, ipsum.</h2>
+                    <h3>Lorem, ipsum.</h3>
                     <p>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quo laudantium magnam ipsam nostrum dolorum, at porro similique
                         in delectus harum eos necessitatibus veniam quis laboriosam quos labore commodi? Maxime?
                     </p>
                 </InfoSection>
                 <InfoSection>
-                    <h2>Lorem, ipsum.</h2>
+                    <h3>Lorem, ipsum.</h3>
                     <p>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quo laudantium magnam ipsam nostrum dolorum, at porro similique
                         in delectus harum eos necessitatibus veniam quis laboriosam quos labore commodi? Maxime?
                     </p>
                 </InfoSection>
                 <InfoSection>
-                    <h2>Lorem, ipsum.</h2>
+                    <h3>Lorem, ipsum.</h3>
                     <p>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quo laudantium magnam ipsam nostrum dolorum, at porro similique
                         in delectus harum eos necessitatibus veniam quis laboriosam quos labore commodi? Maxime?
